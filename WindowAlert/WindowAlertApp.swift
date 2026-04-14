205cd4e86e34bac50827bcca06a1a3bba6da333a
Dan Susman <frenchminister@gmail.com>
2026-04-14 08:40:43 -0400

    Initial Commit

---
+++ WindowAlert/WindowAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindowAlertApp.swift
+//  WindowAlert
+//
+//  Created by Daniel Susman on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct WindowAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
